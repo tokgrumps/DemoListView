d5557c99c4186db22a10d8c3e57442cd2ce6e927
MUHAMMAD JUHAN B JUFRI <15017395@rp.edu.sg>
2017-04-28 12:32:59 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demolistview/Food.java
@@ -0,0 +1,20 @@
+package com.myapplicationdev.android.demolistview;
+
+/**
+ * Created by 15017395 on 28/4/2017.
+ */
+
+public class Food {
+    private String name;
+    private boolean star;
+    public Food(String name, boolean star) {
+        this.name = name;
+        this.star = star;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isStar() {
+        return star;
+    }
+}
